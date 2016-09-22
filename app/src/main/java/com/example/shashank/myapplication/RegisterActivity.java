0395a78207506738ad753fb88820f785ec71431f
CrazyByDefault <es16btech11025@iith.ac.in>
2016-09-23 02:04:35 +0530

    Modified the register page

--- app/src/main/java/com/example/shashank/myapplication/RegisterActivity.java
+++ app/src/main/java/com/example/shashank/myapplication/RegisterActivity.java
@@ -1,7 +1,11 @@
 package com.example.shashank.myapplication;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        final Intent mainActivity = new Intent(this, MapsActivity.class);
+
+        FloatingActionButton register = (FloatingActionButton) findViewById(R.id.registerFab);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(mainActivity);
+
+            }
+        });
+
     }
 }

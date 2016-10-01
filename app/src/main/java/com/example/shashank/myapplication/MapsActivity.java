f15fa8cb028bffb56c17d612fd3326e78b0718cb
CrazyByDefault <es16btech11025@iith.ac.in>
2016-10-01 14:53:09 +0530

    Added circle to map around my location

--- app/src/main/java/com/example/shashank/myapplication/MapsActivity.java
+++ app/src/main/java/com/example/shashank/myapplication/MapsActivity.java
@@ -2,6 +2,7 @@ package com.example.shashank.myapplication;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.location.Location;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentActivity;
@@ -40,12 +41,23 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap map) {
         GPSTracker myLocation = new GPSTracker(this);
         myLocation.getLocation();
-        LatLng myLatLng = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
+        double myLat = myLocation.getLatitude();
+        double myLong = myLocation.getLongitude();
+        LatLng myLatLng = new LatLng(myLat, myLong);
+
+        CircleOptions circleOptions = new CircleOptions();
+        circleOptions.center(myLatLng);
+        circleOptions.radius(20);
+        circleOptions.strokeColor(Color.RED);
+        circleOptions.fillColor(0x30C62828);
+
+
         map.addMarker(new MarkerOptions()
                 .position(new LatLng(17.444792, 78.348310))
                 .title("Megathon")
                 .snippet("Free Hackathon by IIT and IIIT"));
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(17.444792, 78.348310), 10));
+        map.addCircle(circleOptions);
         map.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
             @Override
             public void onInfoWindowClick(Marker marker) {
